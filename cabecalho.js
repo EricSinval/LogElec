7a8b711230b77f90ad20278f03b80e3e5560e2fd
maiconsilveiara <maiconsilveira2017@gmail.com>
2025-09-25 12:06:50 -0300

    Adicionando arquivos da pasta antes do pull

--- cabecalho.js
+++
@@ -1,8 +0,0 @@
-function toggleMenu() {
-    var dropdown = document.getElementById("dropdown");
-    if (dropdown.style.display === "flex") {
-        dropdown.style.display = "none";
-    } else {
-        dropdown.style.display = "flex";
-    }
-}
